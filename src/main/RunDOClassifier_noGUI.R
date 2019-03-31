f015cf3e73633ce00dd26b2be5e846e0a5643299
kohjim <kohji.muraoka@gmail.com>
2019-03-31 15:58:23 +0900

    example running without GUI
    Set wd to "main"

--- src/main/RunDOClassifier_noGUI.R
+++ src/main/RunDOClassifier_noGUI.R
@@ -1,18 +1,25 @@
-source("processUserData.R")
-source("modelMaker.R")
-
-# data used
-inFile <- "example.doobs"
-
-word_length = 4
-alphabet_number = 3
-
-myModel <- modelMaker(word_length,alphabet_number,0)
-
-out <- processUserData(
-  inFile,
-  myModel, # (input$bin_size, input$alphabet_size),
-  word_length, # input$bin_size,
-  alphabet_number, # input$alphabet_size,
-  isWrite = FALSE
-)
+RunDOClassifier_noGUI <- function(inFileName, word_length, alphabet_number){
+  
+  source("processUserData.R")
+  source("modelMaker.R")
+  
+  if (missing(word_length)){
+    word_length = 4
+  }
+  
+  if (missing(alphabet_number)){
+    alphabet_number = 3
+  }
+  
+  myModel <- modelMaker(word_length,alphabet_number,0)
+  
+  out <- processUserData(
+    inFile,
+    myModel, # (input$bin_size, input$alphabet_size),
+    word_length, # input$bin_size,
+    alphabet_number, # input$alphabet_size,
+    isWrite = FALSE
+  )
+  
+  return(out)
+}
---
+++ src/main/example_without_GUI.R
@@ -0,0 +1,6 @@
+# example running a model without GUI
+source("RunDOClassifier_noGUI.R")
+
+# run the model
+results <- RunDOClassifier_noGUI("example.doobs")
+
